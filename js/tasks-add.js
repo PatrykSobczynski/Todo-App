343b1d9fdfc1d704b0f4a3ef3c254dc3e0825ae4
Patryk Sobczynski <patryksobczynski2@wp.pl>
2022-08-06 00:23:33 +0200

    CRUD - task

--- js/tasks-add.js
+++ js/tasks-add.js
@@ -2,32 +2,78 @@ const addBar = document.querySelector(".addInput");
 const addButton = document.querySelector(".addButton");
 const tasks = document.querySelector(".tasks");
 
-// const editTask = document.querySelector(".editTask");
-// const taskDone = document.querySelector(".taskDone");
-// const deleteTask = document.querySelector(".deleteTask");
-
 function addNewTask(value) {
+    // if value in bar is null or blank, dont do anythink
     if (value === "") {
         console.log("pusta wartość");
         return;
     } else {
-        console.log(value);
-
+        // Date, when task was added
         const date = new Date();
-        const task = document.createElement("div");
-        task.classList.add("task");
-        task.innerHTML =
-            `<p class="taskContent">${value}</p>
-            <i class="editTask fa-solid fa-pen-to-square"></i>
-            <i class="taskDone fa-solid fa-check"></i>
-            <i class="deleteTask fa-solid fa-xmark"></i>
-            <p class="dateTaskAdded">
-                ${("0" + date.getDate()).slice(-2)}.${("0" + date.getMonth()).slice(-2)}.${date.getFullYear()}
-            </p>`;
 
-        tasks.appendChild(task);
+        // New Task created
+        const newTask = document.createElement("div");
+        newTask.classList.add("task");
+
+        // creating elements inside new task
+        const pValue = document.createElement("p");
+        const tEdit = document.createElement("i");
+        const tDone = document.createElement("i");
+        const tDelete = document.createElement("i");
+        const pDate = document.createElement("p");
+
+        // adding class to new elements
+        pValue.classList.add("taskContent");
+        tEdit.classList.add("editTask", "fa-solid", "fa-pen-to-square")
+        tDone.classList.add("taskDone", "fa-solid", "fa-check");
+        tDelete.classList.add("deleteTask", "fa-solid", "fa-xmark");
+        pDate.classList.add("dateTaskAdded");
+
+        pValue.innerHTML = value
+        pDate.textContent = `${("0" + date.getDate()).slice(-2)}.${("0" + date.getMonth()).slice(-2)}.${date.getFullYear()}`;
+
+        tasks.appendChild(newTask);
+        newTask.appendChild(pValue);
+        newTask.appendChild(tEdit);
+        newTask.appendChild(tDone);
+        newTask.appendChild(tDelete);
+        newTask.appendChild(pDate);
+
         addBar.value = "";
+
+        tEdit.addEventListener("click", () => {
+            editTask(newTask, pValue, value);
+        })
+
+        tDone.addEventListener("click", () => {
+            pValue.classList.toggle("taskFinished");
+            tDone.classList.toggle("taskFinished-btn");
+        });
+
+        tDelete.addEventListener("click", () => {
+            tasks.removeChild(newTask);
+        })
     }
+}
+
+function editTask(task, contentToModify, content) {
+
+    contentToModify.innerHTML = `
+            <form>
+            <input class = "newValue" value="${content}">
+            <input class = "submitButton" type="submit" value="Modify">
+            </form>`;
+
+    const submitButton = document.querySelector(".submitButton");
+    const newValue = document.querySelector(".newValue");
+
+    submitButton.addEventListener("click", (e) => {
+        contentToModify.innerHTML = newValue.value;
+        if (contentToModify.innerHTML === "") {
+            tasks.removeChild(task);
+        }
+        e.preventDefault();
+    })
 }
 
 addButton.addEventListener("click", (e) => {
@@ -35,5 +81,13 @@ addButton.addEventListener("click", (e) => {
     e.preventDefault();
 });
 
+
 // todo
-// All buttons on task, modify, finished, delete
+// Memory in local storage
+// Task list is downloadable
+// Animation after task deleted/finished
+// Maybe, if is only 1 task, bottom-border doeasn't exist
+
+// bugs
+// if you edited task, and you want to edit it again,
+// you will see old content value on modify-area
