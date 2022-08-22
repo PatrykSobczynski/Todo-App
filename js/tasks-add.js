a91b751551c7d35201498e81ce66c5808ed960c0
Patryk Sobczynski <patryksobczynski2@wp.pl>
2022-08-23 00:59:55 +0200

    start rebuilding the task object - task's div has changed to class + added TasksList

--- js/tasks-add.js
+++ js/tasks-add.js
@@ -1,6 +1,7 @@
 const addBar = document.querySelector(".addInput");
 const addButton = document.querySelector(".addButton");
 const tasks = document.querySelector(".tasks");
+const ul = document.querySelector(".tasks ul");
 
 // TaskInfo actions
 const taskAdded = document.querySelector(".taskAddedConfirm");
@@ -8,129 +9,160 @@ const taskEdit = document.querySelector(".taskEditConfirm");
 const taskDone = document.querySelector(".taskDoneConfirm");
 const taskDeleted = document.querySelector(".taskDeletedConfirm");
 
+const tasksList = [];
+
+// Task class object
+class Task {
+    constructor(value) {
+        this.value = value;
+        this.date = new Date();
+        this.isDone = false;
+        // this.edit = editTask;
+    }
+}
 
 function addNewTask(value) {
-    // if value in bar is null or blank, dont do anythink
     if (value === "") {
-        console.log("pusta wartość");
+        console.log("empty value");
         return;
     } else {
-        // Date, when task was added
-        const date = new Date();
+        const createTask = new Task(value);
+        tasksList.push(createTask);
+        const task = document.createElement("li");
+        task.innerHTML = createTask.value;
+        task.classList.add("task");
+        ul.appendChild(task);
 
-        // New Task creating
-        const newTask = document.createElement("div");
-        newTask.classList.add("task");
-
-        // Creating elements inside new task
-        const pValue = document.createElement("p");
-        const optionsDiv = document.createElement("div");
-        const tEdit = document.createElement("i");
-        const tDone = document.createElement("i");
-        const tDelete = document.createElement("i");
-        const pDate = document.createElement("p");
-
-        // Adding class to new elements
-        pValue.classList.add("taskContent");
-        optionsDiv.classList.add("optionsDiv");
-        tEdit.classList.add("editTask", "fa-solid", "fa-pen-to-square");
-        tDone.classList.add("taskDone", "fa-solid", "fa-check");
-        tDelete.classList.add("deleteTask", "fa-solid", "fa-xmark");
-        pDate.classList.add("dateTaskAdded");
-
-        // Automaticly adding dot
-        const val = value[value.length - 1];
-        if (val != "." && val != "!" && val != "?") {
-            value += ".";
-        }
-
-        pValue.innerHTML = value;
-        pDate.textContent = `${("0" + date.getDate()).slice(-2)}.${("0" + date.getMonth()).slice(-2)}.${date.getFullYear()}`;
-
-        // Adding elements to newTask div
-        tasks.appendChild(newTask);
-
-        newTask.appendChild(pValue);
-        newTask.appendChild(optionsDiv);
-
-        optionsDiv.appendChild(tEdit);
-        optionsDiv.appendChild(tDone);
-        optionsDiv.appendChild(tDelete);
-        optionsDiv.appendChild(pDate);
-
-        // Clearing input
         addBar.value = "";
 
         // Animations
         taskInfo(taskAdded);
-        AddTaskAnimation(newTask);
-
-        // Do somethink after click
-        tEdit.addEventListener("click", () => {
-            editTask(newTask, pValue);
-            // TaskInfo edited below, on modify button
-        })
-
-        tDone.addEventListener("click", () => {
-            pValue.classList.toggle("taskFinished");
-            tDone.classList.toggle("taskFinished-btn");
-            taskInfo(taskDone);
-        });
-
-        tDelete.addEventListener("click", () => {
-            newTask.style.animation = "taskRemoveAnimation .6s linear both"
-            setTimeout(() => {
-                tasks.removeChild(newTask);
-                taskInfo(taskDeleted);
-            }, 650);
-        })
+        AddTaskAnimation(task);
     }
 }
 
-function editTask(task, contentToModify) {
+// function addNewTask(value) {
+//     // if value in bar is null or blank, dont do anythink
+//     if (value === "") {
+//         console.log("pusta wartość");
+//         return;
+//     } else {
+//         // Date, when task was added
+//         const date = new Date();
 
-    const taskChilds = document.querySelectorAll(".task > *:not(:first-child)");
-    content = contentToModify.textContent;
+//         // New Task creating
+//         const newTask = document.createElement("div");
+//         newTask.classList.add("task");
+
+//         // Creating elements inside new task
+//         const pValue = document.createElement("p");
+//         const optionsDiv = document.createElement("div");
+//         const tEdit = document.createElement("i");
+//         const tDone = document.createElement("i");
+//         const tDelete = document.createElement("i");
+//         const pDate = document.createElement("p");
+
+//         // Adding class to new elements
+//         pValue.classList.add("taskContent");
+//         optionsDiv.classList.add("optionsDiv");
+//         tEdit.classList.add("editTask", "fa-solid", "fa-pen-to-square");
+//         tDone.classList.add("taskDone", "fa-solid", "fa-check");
+//         tDelete.classList.add("deleteTask", "fa-solid", "fa-xmark");
+//         pDate.classList.add("dateTaskAdded");
+
+//         // Automaticly adding dot
+//         const val = value[value.length - 1];
+//         if (val != "." && val != "!" && val != "?") {
+//             value += ".";
+//         }
+
+//         pValue.innerHTML = value;
+//         pDate.textContent = `${("0" + date.getDate()).slice(-2)}.${("0" + date.getMonth()).slice(-2)}.${date.getFullYear()}`;
+
+//         // Adding elements to newTask div
+//         tasks.appendChild(newTask);
+
+//         newTask.appendChild(pValue);
+//         newTask.appendChild(optionsDiv);
+
+//         optionsDiv.appendChild(tEdit);
+//         optionsDiv.appendChild(tDone);
+//         optionsDiv.appendChild(tDelete);
+//         optionsDiv.appendChild(pDate);
+
+//         // Clearing input
+//         addBar.value = "";
+
+//         // Animations
+//         taskInfo(taskAdded);
+//         AddTaskAnimation(newTask);
+
+//         // Do somethink after click
+//         tEdit.addEventListener("click", () => {
+//             editTask(newTask, pValue);
+//             // TaskInfo edited below, on modify button
+//         })
+
+//         tDone.addEventListener("click", () => {
+//             pValue.classList.toggle("taskFinished");
+//             tDone.classList.toggle("taskFinished-btn");
+//             taskInfo(taskDone);
+//         });
+
+//         tDelete.addEventListener("click", () => {
+//             newTask.style.animation = "taskRemoveAnimation .6s linear both"
+//             setTimeout(() => {
+//                 tasks.removeChild(newTask);
+//                 taskInfo(taskDeleted);
+//             }, 650);
+//         })
+//     }
+// }
+
+// function editTask(task, contentToModify) {
+
+//     const taskChilds = document.querySelectorAll(".task > *:not(:first-child)");
+//     content = contentToModify.textContent;
 
 
 
-    // Editing tasks from
-    taskChilds.forEach(taskChild => {
-        taskChild.classList.add("hideTaskOptions");
-    })
-    contentToModify.innerHTML = `
-            <form>
-            <input class = "newValue" value="${content}">
-            <input class = "submitButton" type="submit" value="Modify">
-            </form>`;
+//     // Editing tasks from
+//     taskChilds.forEach(taskChild => {
+//         taskChild.classList.add("hideTaskOptions");
+//     })
+//     contentToModify.innerHTML = `
+//             <form>
+//             <input class = "newValue" value="${content}">
+//             <input class = "submitButton" type="submit" value="Modify">
+//             </form>`;
 
-    const submitButton = document.querySelector(".submitButton");
-    const newValue = document.querySelector(".newValue");
+//     const submitButton = document.querySelector(".submitButton");
+//     const newValue = document.querySelector(".newValue");
 
-    submitButton.addEventListener("click", (e) => {
-        editTaskAnimation(task);
-        setTimeout(() => {
-            // Changing task value
-            content = newValue.value;
-            contentToModify.innerHTML = newValue.value;
+//     submitButton.addEventListener("click", (e) => {
+//         editTaskAnimation(task);
+//         setTimeout(() => {
+//             // Changing task value
+//             content = newValue.value;
+//             contentToModify.innerHTML = newValue.value;
 
-            // If task value is equal null or empty, task will remove
-            if (contentToModify.innerHTML === "") {
-                tasks.removeChild(task);
-                taskInfo(taskDeleted);
-            }
-            else {
-                taskInfo(taskEdit);
-            }
-        }, 300);
+//             // If task value is equal null or empty, task will remove
+//             if (contentToModify.innerHTML === "") {
+//                 tasks.removeChild(task);
+//                 taskInfo(taskDeleted);
+//             }
+//             else {
+//                 taskInfo(taskEdit);
+//             }
+//         }, 300);
 
-        taskChilds.forEach(taskChild => {
-            taskChild.classList.remove("hideTaskOptions");
-        })
+//         taskChilds.forEach(taskChild => {
+//             taskChild.classList.remove("hideTaskOptions");
+//         })
 
-        e.preventDefault();
-    })
-}
+//         e.preventDefault();
+//     })
+// }
 
 // ANIMATIONS ⬇ ⬇ ⬇ ⬇
 function taskInfo(taskAction) {
@@ -159,40 +191,40 @@ addButton.addEventListener("click", (e) => {
 });
 
 
-// Confirm closing window
-window.onbeforeunload = function (e) {
-    const warningMessage = "Tasks will not be saved, are you sure you want to close this window?";
-    e = e || window.event;
+// // Confirm closing window
+// window.onbeforeunload = function (e) {
+//     const warningMessage = "Tasks will not be saved, are you sure you want to close this window?";
+//     e = e || window.event;
 
-    if (e) {
-        e.returnValue = warningMessage;
-    }
-    return warningMessage;
-}
+//     if (e) {
+//         e.returnValue = warningMessage;
+//     }
+//     return warningMessage;
+// }
 
 // Tasks to PDF
-function downloadTasks() {
-    const options = {
-        margin: 10,
-        filename: "Tasks.pdf",
-    }
-    const taskList = document.querySelectorAll(".task .taskContent");
-    const downloadList = ["Tasks: "];
+// function downloadTasks() {
+//     const options = {
+//         margin: 10,
+//         filename: "Tasks.pdf",
+//     }
+//     const taskList = document.querySelectorAll(".task .taskContent");
+//     const downloadList = ["Tasks: "];
 
-    taskList.forEach(task => {
-        downloadList.push(task.textContent);
-    })
+//     taskList.forEach(task => {
+//         downloadList.push(task.textContent);
+//     })
 
-    // new line brake doens't work in html2pdf 
-    // const listJoined = downloadList.join("\n");
-    const listJoined = downloadList.join(" ");
+//     // new line brake doens't work in html2pdf
+//     // const listJoined = downloadList.join("\n");
+//     const listJoined = downloadList.join(" ");
 
-    console.log(listJoined);
-    window.html2pdf().set(options).from(listJoined).save();
-}
+//     console.log(listJoined);
+//     window.html2pdf().set(options).from(listJoined).save();
+// }
 
-const downloadTasksBtn = document.querySelector(".downloadTasks");
-downloadTasksBtn.addEventListener("click", downloadTasks);
+// const downloadTasksBtn = document.querySelector(".downloadTasks");
+// downloadTasksBtn.addEventListener("click", downloadTasks);
 
 // todo
 // Memory in local storage
