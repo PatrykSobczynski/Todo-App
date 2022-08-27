25aff859e5225fd6070e938efed936db01e81a77
Patryk Sobczynski <patryksobczynski2@wp.pl>
2022-08-27 14:29:54 +0200

    Add changeStatus function and deleteTask Function, ShowList and renderTasks now are one function

--- js/tasks-add.js
+++ js/tasks-add.js
@@ -9,7 +9,10 @@ const taskEdit = document.querySelector(".taskEditConfirm");
 const taskDone = document.querySelector(".taskDoneConfirm");
 const taskDeleted = document.querySelector(".taskDeletedConfirm");
 
-const tasksList = JSON.parse(localStorage.getItem("Task")) || [];
+const taskStatusBtn = [...document.querySelectorAll(".task .taskDone")];
+const taskDeleteBtn = [...document.querySelectorAll(".task .deleteTask")]
+
+let tasksList = JSON.parse(localStorage.getItem("Task")) || [];
 
 const date = new Date();
 
@@ -19,7 +22,6 @@ class Task {
         this.value = value;
         this.date = date;
         this.isDone = false;
-        // this.edit = editTask;
     }
 }
 
@@ -29,7 +31,9 @@ function addNewTask(value) {
         return;
     } else {
         const date = new Date();
-        const taskCreatedDate = `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
+        const taskCreatedDate = `
+            <span class="time">${("0" + date.getHours()).slice(-2)}:${("0" + date.getMinutes()).slice(-2)}</span>
+            <span class="date">${("0" + date.getDate()).slice(-2)}.${("0" + (date.getMonth() + 1)).slice(-2)}.${date.getFullYear()}</span>`;
 
         // Automaticly adding dot
         const val = value[value.length - 1];
@@ -38,167 +42,72 @@ function addNewTask(value) {
         }
 
         const createTask = new Task(value, taskCreatedDate);
-
         tasksList.push(createTask);
-
-        const task = document.createElement("li");
-        task.innerHTML = renderTasks(createTask);
-        task.classList.add("task");
-        ul.appendChild(task);
 
         addBar.value = "";
 
         // Save task in browser's memory
         localStorage.setItem("Task", JSON.stringify(tasksList))
 
+        ShowList(tasksList, ul);
+
         // Animations
         taskInfo(taskAdded);
-        AddTaskAnimation(task);
+        // AddTaskAnimation(task);
+
     }
 }
 
 function ShowList(tasks = [], tasksHolder) {
-    tasksHolder.innerHTML = tasks.map((task) => {
+    tasksHolder.innerHTML = tasks.map((task, i) => {
         return `
-        <li class="task task-active">
-            ${renderTasks(task)}
+        <li class="task task-active" data-id=${i}>
+            <p class="taskContent ${task.isDone ? 'taskFinished' : ''}">${task.value}</p>
+            <div class="optionsDiv">
+                <i class="editTask fa-solid fa-pen-to-square"></i>
+                <i class="taskDone ${task.isDone ? 'taskFinished-btn' : ''} fa-solid fa-check"></i>
+                <i class="deleteTask fa-solid fa-xmark"></i>
+                <p class="dateTaskAdded">${task.date}</p>
+            </div>
         </li>`
     }).join('');
 }
 
-function renderTasks(task) {
-    return `
-        <p class="taskContent">${task.value}</p>
-        <div class="optionsDiv">
-            <p>${task.date}</p>
-        </div>
-    `
+function changeTaskStatus(e) {
+    const elementParent = e.target.parentElement.parentElement;
+    const id = elementParent.dataset.id;
+
+    tasksList[id].isDone = !tasksList[id].isDone;
+    localStorage.setItem("Task", JSON.stringify(tasksList));
+
+    console.log(tasksList[id].value, tasksList[id].isDone);
+
+    ShowList(tasksList, ul);
+    taskInfo(taskDone);
+}
+
+function deleteTask(e) {
+    const elementParent = e.target.parentElement.parentElement;
+    const id = elementParent.dataset.id;
+
+    // second param is 1, because we want to delete only one element
+    tasksList.splice(id, 1);
+
+    localStorage.setItem("Task", JSON.stringify(tasksList));
+
+    ShowList(tasksList, ul);
+    taskInfo(taskDeleted);
 }
 
 ShowList(tasksList, ul);
 
-// function addNewTask(value) {
-//     // if value in bar is null or blank, dont do anythink
-//     if (value === "") {
-//         console.log("pusta wartość");
-//         return;
-//     } else {
-//         // Date, when task was added
-//         const date = new Date();
+taskStatusBtn.forEach(task => {
+    task.addEventListener("click", changeTaskStatus);
+})
 
-//         // New Task creating
-//         const newTask = document.createElement("div");
-//         newTask.classList.add("task");
-
-//         // Creating elements inside new task
-//         const pValue = document.createElement("p");
-//         const optionsDiv = document.createElement("div");
-//         const tEdit = document.createElement("i");
-//         const tDone = document.createElement("i");
-//         const tDelete = document.createElement("i");
-//         const pDate = document.createElement("p");
-
-//         // Adding class to new elements
-//         pValue.classList.add("taskContent");
-//         optionsDiv.classList.add("optionsDiv");
-//         tEdit.classList.add("editTask", "fa-solid", "fa-pen-to-square");
-//         tDone.classList.add("taskDone", "fa-solid", "fa-check");
-//         tDelete.classList.add("deleteTask", "fa-solid", "fa-xmark");
-//         pDate.classList.add("dateTaskAdded");
-
-//         // Automaticly adding dot
-//         const val = value[value.length - 1];
-//         if (val != "." && val != "!" && val != "?") {
-//             value += ".";
-//         }
-
-//         pValue.innerHTML = value;
-//         pDate.textContent = `${("0" + date.getDate()).slice(-2)}.${("0" + date.getMonth()).slice(-2)}.${date.getFullYear()}`;
-
-//         // Adding elements to newTask div
-//         tasks.appendChild(newTask);
-
-//         newTask.appendChild(pValue);
-//         newTask.appendChild(optionsDiv);
-
-//         optionsDiv.appendChild(tEdit);
-//         optionsDiv.appendChild(tDone);
-//         optionsDiv.appendChild(tDelete);
-//         optionsDiv.appendChild(pDate);
-
-//         // Clearing input
-//         addBar.value = "";
-
-//         // Animations
-//         taskInfo(taskAdded);
-//         AddTaskAnimation(newTask);
-
-//         // Do somethink after click
-//         tEdit.addEventListener("click", () => {
-//             editTask(newTask, pValue);
-//             // TaskInfo edited below, on modify button
-//         })
-
-//         tDone.addEventListener("click", () => {
-//             pValue.classList.toggle("taskFinished");
-//             tDone.classList.toggle("taskFinished-btn");
-//             taskInfo(taskDone);
-//         });
-
-//         tDelete.addEventListener("click", () => {
-//             newTask.style.animation = "taskRemoveAnimation .6s linear both"
-//             setTimeout(() => {
-//                 tasks.removeChild(newTask);
-//                 taskInfo(taskDeleted);
-//             }, 650);
-//         })
-//     }
-// }
-
-// function editTask(task, contentToModify) {
-
-//     const taskChilds = document.querySelectorAll(".task > *:not(:first-child)");
-//     content = contentToModify.textContent;
-
-
-
-//     // Editing tasks from
-//     taskChilds.forEach(taskChild => {
-//         taskChild.classList.add("hideTaskOptions");
-//     })
-//     contentToModify.innerHTML = `
-//             <form>
-//             <input class = "newValue" value="${content}">
-//             <input class = "submitButton" type="submit" value="Modify">
-//             </form>`;
-
-//     const submitButton = document.querySelector(".submitButton");
-//     const newValue = document.querySelector(".newValue");
-
-//     submitButton.addEventListener("click", (e) => {
-//         editTaskAnimation(task);
-//         setTimeout(() => {
-//             // Changing task value
-//             content = newValue.value;
-//             contentToModify.innerHTML = newValue.value;
-
-//             // If task value is equal null or empty, task will remove
-//             if (contentToModify.innerHTML === "") {
-//                 tasks.removeChild(task);
-//                 taskInfo(taskDeleted);
-//             }
-//             else {
-//                 taskInfo(taskEdit);
-//             }
-//         }, 300);
-
-//         taskChilds.forEach(taskChild => {
-//             taskChild.classList.remove("hideTaskOptions");
-//         })
-
-//         e.preventDefault();
-//     })
-// }
+taskDeleteBtn.forEach(task => {
+    task.addEventListener("click", deleteTask)
+})
 
 // ANIMATIONS ⬇ ⬇ ⬇ ⬇
 function taskInfo(taskAction) {
@@ -227,44 +136,34 @@ addButton.addEventListener("click", (e) => {
 });
 
 
-// // Confirm closing window
-// window.onbeforeunload = function (e) {
-//     const warningMessage = "Tasks will not be saved, are you sure you want to close this window?";
-//     e = e || window.event;
-
-//     if (e) {
-//         e.returnValue = warningMessage;
-//     }
-//     return warningMessage;
-// }
-
 // Tasks to PDF
-// function downloadTasks() {
-//     const options = {
-//         margin: 10,
-//         filename: "Tasks.pdf",
-//     }
-//     const taskList = document.querySelectorAll(".task .taskContent");
-//     const downloadList = ["Tasks: "];
+function downloadTasks() {
+    const options = {
+        margin: 10,
+        filename: "Tasks.pdf",
+    }
+    const taskList = document.querySelectorAll(".task .taskContent");
+    const downloadList = ["Tasks: "];
 
-//     taskList.forEach(task => {
-//         downloadList.push(task.textContent);
-//     })
+    taskList.forEach(task => {
+        downloadList.push(task.textContent);
+    })
 
-//     // new line brake doens't work in html2pdf
-//     // const listJoined = downloadList.join("\n");
-//     const listJoined = downloadList.join(" ");
+    // const listJoined = downloadList.join("\n");
+    const listJoined = downloadList.join(" ");
 
-//     console.log(listJoined);
-//     window.html2pdf().set(options).from(listJoined).save();
-// }
+    console.log(listJoined);
+    window.html2pdf().set(options).from(listJoined).save();
+}
 
-// const downloadTasksBtn = document.querySelector(".downloadTasks");
-// downloadTasksBtn.addEventListener("click", downloadTasks);
+const downloadTasksBtn = document.querySelector(".downloadTasks");
+downloadTasksBtn.addEventListener("click", downloadTasks);
 
 // todo
-// Memory in local storage
+// Edit task function
+// PDF downloader - new line brake doens't work in html2pdf. Figure out something
 // Maybe, use only 1 info div, and after click, change the textContent and add/remove class
 
 // bugs
-// 1. if there are 2 modify-area open, and you click modify, the page will refresh
+// 1. You can only one time click button, if you want to click again it doesn't work
+//      untill you refresh the website
